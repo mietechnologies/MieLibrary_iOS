b75974c912edb28dc8e591efb755487d831df5d2
Brett Chapin <brett.chapin89@gmail.com>
2024-10-21 21:35:55 -0400

    Created SortingCategories model for storing and representing how the books will be sorted

---
+++ MieLibrary/Models/SortingCategories.swift
@@ -0,0 +1,48 @@
+//
+//  SortingCategories.swift
+//  MieLibrary
+//
+//  Created by Brett Chapin on 10/19/24.
+//
+
+import Foundation
+
+enum SortingCategories: String, CaseIterable {
+    case title = "Title"
+    case author = "Author"
+    case dateAdded = "Date Added"
+    case publisher = "Publisher"
+    case genre = "Genre"
+    
+    func sortDescriptors(_ order: SortOrder = .forward) -> [SortDescriptor<Book>] {
+        switch self {
+        case .title:
+            return [
+                SortDescriptor(\Book.title, order: order),
+                SortDescriptor(\Book.subTitle, order: .forward)
+            ]
+        case .author: // TODO: Update for when author name is split
+            return [
+                SortDescriptor(\Book.author, order: order),
+                SortDescriptor(\Book.title, order: .forward),
+                SortDescriptor(\Book.subTitle, order: .forward)
+            ]
+        case .dateAdded:
+            return [
+                SortDescriptor(\Book.dateAdded, order: order)
+            ]
+        case .publisher:
+            return [
+                SortDescriptor(\Book.publisher, order: order),
+                SortDescriptor(\Book.title, order: .forward),
+                SortDescriptor(\Book.subTitle, order: .forward)
+            ]
+        case .genre:
+            return [
+                SortDescriptor(\Book.genre.rawValue, order: order),
+                SortDescriptor(\Book.title, order: .forward),
+                SortDescriptor(\Book.subTitle, order: .forward)
+            ]
+        }
+    }
+}
